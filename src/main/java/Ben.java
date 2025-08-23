4a4c476be91ed3bff61312cf8909d027b162598c
Chenxiao <seanofapollo@gmail.com>
2025-08-23 16:55:27 +0800

    change: Chatbot name Duke -> Ben and added additional welcome and goodbye messages

---
+++ src/main/java/Ben.java
@@ -0,0 +1,17 @@
+public class Ben {
+    public static void main(String[] args) {
+        //variables
+        String name = "Ben";
+        String lineBreak = "____________________________________________________________";
+
+        //Print Welcome Msg
+        System.out.println(lineBreak);
+        System.out.printf(" Hello! I'm %s%n", name);
+        System.out.println(" What can I do for you?");
+        System.out.println(lineBreak);
+
+        //Print Goodbye Msg
+        System.out.println(" Bye. Hope to see you again soon!");
+        System.out.println(lineBreak);
+    }
+}
