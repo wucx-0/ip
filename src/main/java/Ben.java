99fe20dd51fa11194eb9562b71800c2161002d79
Chenxiao <seanofapollo@gmail.com>
2025-08-23 17:23:11 +0800

    Implement Level-1: Echo functionality

--- src/main/java/Ben.java
+++ src/main/java/Ben.java
@@ -1,6 +1,9 @@
+import java.util.Scanner;
+
 public class Ben {
     public static void main(String[] args) {
         //variables
+        Scanner scanner = new Scanner(System.in);
         String name = "Ben";
         String lineBreak = "____________________________________________________________";
 
@@ -10,8 +13,27 @@ public class Ben {
         System.out.println(" What can I do for you?");
         System.out.println(lineBreak);
 
+        //while loop that echoes input
+
+        String input;
+        while (true) {
+            input = scanner.nextLine();
+
+            if (input.equals("bye")) {
+                break;
+            }
+
+            System.out.println(lineBreak);
+            System.out.printf(" %s%n", input);
+            System.out.println(lineBreak);
+
+        }
+
         //Print Goodbye Msg
-        System.out.println(" Bye. Hope to see you again soon!");
         System.out.println(lineBreak);
+        System.out.println(" Bye! Hope to see you again soon!");
+        System.out.println(lineBreak);
+
+        scanner.close();
     }
 }
