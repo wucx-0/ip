aac1273cc18c300e9bc8a31f0d69c833cfc281ab
Chenxiao <seanofapollo@gmail.com>
2025-09-01 17:45:29 +0800

    add Storage.java and modify task subclasses

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -8,6 +8,14 @@ class Event extends Task {
         this.to = to;
     }
 
+    public String getFrom() {
+        return this.from;
+    }
+
+    public String getTo() {
+        return this.to;
+    }
+
     @Override
     public String getType() {
         return "E";
